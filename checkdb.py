808d692c21877e4e6fd59953446d7959aa66f977
Daniil Furkalo <daniilf077@gmail.com>
2025-04-09 13:49:27 +0300

    fix

---
+++ checkdb.py
@@ -0,0 +1,40 @@
+import sqlite3
+conn = sqlite3.connect('db.sqlite3')  # Замініть на шлях до вашого SQLite файлу
+cursor = conn.cursor()
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")  # Наприклад, щоб побачити всі таблиці
+tables = cursor.fetchall()
+print(tables)
+
+# Для кожної таблиці отримуємо і виводимо всі дані
+for table in tables:
+    table_name = table[0]
+    print(f"Таблиця: {table_name}")
+    cursor.execute(f"SELECT * FROM {table_name}")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    print("-" * 50)  # Розділювач між таблицями
+
+
+cursor.execute("PRAGMA table_info(shop_category);")
+# Отримання результатів
+columns = cursor.fetchall()
+
+# Виведення результатів
+for column in columns:
+    print(column)
+
+
+print(f"Таблиця: shop_product")
+
+cursor.execute("PRAGMA table_info(shop_product);")
+# Отримання результатів
+columns = cursor.fetchall()
+
+
+# Виведення результатів
+for column in columns:
+    print(column)
+
+
+conn.close()
