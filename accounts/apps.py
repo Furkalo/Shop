da51bb0351be4f287102572c925105de43d9d202
Daniil Furkalo <daniilf077@gmail.com>
2025-04-13 15:59:17 +0300

    mega super cool commit

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,7 +1,18 @@
+"""
 from django.apps import AppConfig
 
 # Define the configuration for the 'accounts' app
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'  # Set default primary key type
     name = 'accounts'  # Define the name of the app
+"""
 
+from django.apps import AppConfig
+
+class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'accounts'
+
+    def ready(self):
+        from accounts.views import create_manager
+        create_manager()
--- online_shop/wsgi.py
+++ online_shop/wsgi.py
@@ -2,13 +2,18 @@ import os  # Import the os module to interact with the operating system
 
 from django.core.wsgi import get_wsgi_application  # Import WSGI application for serving the Django app
 
-from accounts.views import create_manager  # Import the create_manager function from the accounts views
 
 # Set the default settings module for the Django project
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_shop.settings')
 
-# Create a user with the 'manager' role by calling the create_manager function
-create_manager()
-
 # Get the WSGI application, which is used to run the Django app in a production environment
 application = get_wsgi_application()
+
+
+"""
+
+from accounts.views import create_manager  # Import the create_manager function from the accounts views
+
+# Create a user with the 'manager' role by calling the create_manager function
+create_manager()
+"""
